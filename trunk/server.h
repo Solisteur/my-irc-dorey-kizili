c288a0fbacd0f36317eb173a9bcecf730e425055
nailizik@gmail.com <nailizik@gmail.com@07ac0b6e-7741-964b-43b3-98990cb66f5c>
2011-04-23 10:51:43 +0000

    server fonctionnel, manque les commandes et le parseur de commandes

--- trunk/server.h
+++ trunk/server.h
@@ -5,7 +5,7 @@
 ** Login   <kizili_g@epitech.net>
 ** 
 ** Started on  Mon Apr 18 15:02:04 2011 guillaume kizilian
-** Last update Tue Apr 19 15:54:20 2011 guillaume kizilian
+** Last update Thu Apr 21 17:26:00 2011 guillaume kizilian
 */
 
 #ifndef			_SERVER_H_
@@ -14,31 +14,38 @@
 # define		TITLE_SIZE 255
 # define		NAME_SIZE 55
 # define		MAX_CHANNEL 5
+# define		DEFAULT_MAX_USERS 10
+# define		DEFAULT_CHAN_BUFF_SIZE 4096
+
+#include		<sys/types.h>
+#include		<sys/socket.h>
 
 typedef struct		s_user
 {
   char			name[NAME_SIZE + 1];
   int			id;
+  int			cs;
   struct s_user		*n;
 }			t_user;
 
 typedef struct		s_chan
 {
   char			title[TITLE_SIZE + 1];
-  int			id;
   int			u_max;
+  char			buff[DEFAULT_CHAN_BUFF_SIZE];
+  int			buff_size;
   t_user		*users;
-  struct s_chan		*n;
-}
+}			t_chan;
 
 typedef struct		s_serv
 {
   char			title[TITLE_SIZE + 1];
-  int			c_max;
-  t_chan		*chan;
+  t_chan		chan[MAX_CHANNEL];
   int			s;
   struct sockaddr_in	*sin;
   int			port;
+  struct sockaddr_in	*csin;
+  int			csize;
 }			t_serv;
 
 #endif
